30e115dadc61f73baeb33ea7799c551e2a99e6d7
Hieu Le <lehieu70@gmail.com>
2010-05-03 17:40:26 -0700

    Adding final touch to Help

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 describe PagesController do
   integrate_views
 
+  before(:each)	do
+    #
+    # Define @base_title here
+    @base_title = "Ruby on Rails Tutorial Sample App"
+    #
+  end
 
   describe "GET 'home'" do
 
@@ -14,8 +20,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'home'
-      response.should have_tag("title",
-                   "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_tag("title", @base_title + " | Home")
     end
 
   end
@@ -31,8 +36,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'contact'
-      response.should have_tag("title",
-                "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_tag("title", @base_title + " | Contact") 
     end
 
   end
@@ -47,8 +51,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'about'
-      response.should have_tag("title",
-                    "Ruby on Rails Tutorial Sample App | About")
+      response.should have_tag("title", @base_title + " | About")
     end
 
   end
@@ -63,8 +66,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'help'
-      response.should have_tag("title",
-        "Ruby on Rails Tutorial Sample App | Help")
+      response.should have_tag("title", @base_title + " | Help")
     end
   
   end
